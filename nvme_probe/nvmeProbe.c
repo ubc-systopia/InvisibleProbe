117a902ec0a0f386a19e7b4e11a9fed50fe0ad04
Satvik <vssatvik@cs.ubc.ca>
2025-01-13 10:47:55 -0800

    Slightly modified NVMe probe (for better understanding) and analysis script

--- nvme_probe/nvmeProbe.c
+++ nvme_probe/nvmeProbe.c
@@ -1,163 +1,136 @@
 #include "utils.h"
 
-void Probe(void);
+void run_probe(void);
 
-#define CPU_HZ 3600000000.0 // CHANGE: CPU clock frequency of your machine;in Linux, you can "cat /proc/cpuinfo" to see it;
-#define Dot_Num 0x800000    // CHANGE: the read operations
-#define LOG_NAME "records.bin"
-#define LBA_NUM 8
-#define BUFFER_SIZE (LBA_NUM * 512) // 4KB
-#define READ_LBA 0x10000
-#define CMD_BUFF_NUMS 128
+// CPU clock frequency in Hz (obtainable via `sudo dmesg | grep -i tsc`)
+#define CPU_HZ 4700000000.0
+#define NUM_READ_OPS 0x800000    // Number of read operations
+#define LOG_FILE "records.bin"
+#define LBA_BLOCKS 8
+#define BUFFER_SIZE (LBA_BLOCKS * 512) // 4KB buffer size
+#define READ_START_LBA 0x10000
+#define CMD_QUEUE_LIMIT 256
 
-struct hello_world_sequence {
-	struct ns_entry	*ns_entry;
-	char		*buf;
-	unsigned        using_cmb_io;
-	uint64_t   completed_io;
+struct io_sequence {
+    struct ns_entry *ns_entry;
+    char *buffer;
+    unsigned using_cmb_io;
+    uint64_t completed_io_count;
 };
 
-static void read_complete(void *arg, const struct spdk_nvme_cpl *completion)
+static void handle_read_complete(void *arg, const struct spdk_nvme_cpl *completion)
 {
-	struct hello_world_sequence *sequence = arg;
-	sequence->completed_io ++ ;
-	if (spdk_nvme_cpl_is_error(completion)) {
-		spdk_nvme_qpair_print_completion(sequence->ns_entry->qpair, (struct spdk_nvme_cpl *)completion);
-		fprintf(stderr, "I/O error status: %s\n", spdk_nvme_cpl_get_status_string(&completion->status));
-		fprintf(stderr, "Read I/O failed, aborting run\n");
-	}
+    struct io_sequence *sequence = arg;
+    sequence->completed_io_count++;
+
+    if (spdk_nvme_cpl_is_error(completion)) {
+        spdk_nvme_qpair_print_completion(sequence->ns_entry->qpair, (struct spdk_nvme_cpl *)completion);
+        fprintf(stderr, "I/O error: %s\n", spdk_nvme_cpl_get_status_string(&completion->status));
+        fprintf(stderr, "Read I/O failed, aborting.\n");
+    }
 }
 
+void run_probe(void) {
+    uint64_t *timestamps = spdk_zmalloc(sizeof(uint64_t) * NUM_READ_OPS, 0x1000, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
+    memset(timestamps, 0, sizeof(uint64_t) * NUM_READ_OPS);
+    volatile uint64_t ops_in_progress = 0;
 
-void Probe(void){
-	// alloc memory to log the rdtscp stamp
-    uint64_t* time_line = (uint64_t *)spdk_zmalloc(sizeof(uint64_t)*Dot_Num, 0x1000, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-	memset(time_line, 0, sizeof(uint64_t)*Dot_Num);
-	volatile uint64_t p=0;
-
-	// alloc Q pairs
-	struct ns_entry		*ns_entry = g_namespaces;
+    struct ns_entry *ns_entry = g_namespaces;
     ns_entry->qpair = spdk_nvme_ctrlr_alloc_io_qpair(ns_entry->ctrlr, NULL, 0);
-    if (ns_entry->qpair == NULL) {
-            printf("ERROR: spdk_nvme_ctrlr_alloc_io_qpair() failed\n");
+    if (!ns_entry->qpair) {
+        fprintf(stderr, "Error: Failed to allocate I/O queue pair.\n");
+        return;
     }
 
-    struct hello_world_sequence	sequence;
-    sequence.buf = spdk_zmalloc(BUFFER_SIZE, 0x1000, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA);
-    sequence.buf[0]=1;
-	sequence.completed_io = 0;
-	sequence.ns_entry = ns_entry;
-    
-    int rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buf,
-                            READ_LBA,
-                            LBA_NUM,
-                            read_complete, &sequence, 0);
+    struct io_sequence sequence = {
+        .ns_entry = ns_entry,
+        .buffer = spdk_zmalloc(BUFFER_SIZE, 0x1000, NULL, SPDK_ENV_SOCKET_ID_ANY, SPDK_MALLOC_DMA),
+        .completed_io_count = 0
+    };
+    sequence.buffer[0] = 1;
 
+	// initial read command to discard first time extra-effects.
+    int rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buffer,
+                                   READ_START_LBA, LBA_BLOCKS, handle_read_complete, &sequence, 0);
     if (rc != 0) {
-        fprintf(stderr, "starting read I/O failed\n");
+        fprintf(stderr, "Error: Failed to start read I/O.\n");
         exit(1);
     }
 
-	uint64_t in_processing = 1;
-    uint64_t already_completed_num = 0;
-	uint64_t completion_left=Dot_Num;
+    uint64_t completed_ops = 0;
+    uint64_t remaining_ops = NUM_READ_OPS;
 
-	while (completion_left) {
-		// wait until an operation completes
-		while (sequence.completed_io == already_completed_num) {
-                spdk_nvme_qpair_process_completions(ns_entry->qpair, 1);
-            }
-		// log rdtscp stamps
-        time_line[p++] = rdtscp();
+    while (remaining_ops > 0) {
+        while (sequence.completed_io_count == completed_ops) {
+            spdk_nvme_qpair_process_completions(ns_entry->qpair, 1);
+        }
 
-        completion_left --;
-		in_processing --;
-        already_completed_num++;
+        timestamps[ops_in_progress++] = rdtscp();
+        remaining_ops--;
+        completed_ops++;
 
-		// refill the send Q with read cmds
-		while(in_processing < CMD_BUFF_NUMS && completion_left > 0){
-            rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buf,
-                                    READ_LBA,
-                                    LBA_NUM,
-                                    read_complete, &sequence, 0);
+        while (ops_in_progress < CMD_QUEUE_LIMIT && remaining_ops > 0) {
+            rc = spdk_nvme_ns_cmd_read(ns_entry->ns, ns_entry->qpair, sequence.buffer,
+                                       READ_START_LBA, LBA_BLOCKS, handle_read_complete, &sequence, 0);
             if (rc != 0) {
-                fprintf(stderr, "starting read I/O failed\n");
+                fprintf(stderr, "Error: Failed to start read I/O.\n");
                 exit(1);
             }
-            in_processing++; 
-		}
+            ops_in_progress++;
+        }
     }
-	
-	// handle the left read commands
-	while(in_processing -- ){
-		while (sequence.completed_io == already_completed_num) {
-                spdk_nvme_qpair_process_completions(ns_entry->qpair, 1);
-            }
-		already_completed_num++; 
-	}
-	spdk_nvme_ctrlr_free_io_qpair(ns_entry->qpair);
-    
-    puts("Detect done! Start analyzing..."); 
 
+    while (ops_in_progress-- > 0) {
+        while (sequence.completed_io_count == completed_ops) {
+            spdk_nvme_qpair_process_completions(ns_entry->qpair, 1);
+        }
+        completed_ops++;
+    }
 
-    FILE *fp = fopen(LOG_NAME,"wb");
-	fwrite(time_line, sizeof(uint64_t), Dot_Num, fp);
-    fclose(fp);
+    spdk_nvme_ctrlr_free_io_qpair(ns_entry->qpair);
+    printf("Detection complete. Starting analysis...\n");
 
-    puts("=========Summary==========");
-    uint64_t total_gap = time_line[Dot_Num-1] - time_line[0];
-    double total_gap_time = total_gap/CPU_HZ;
-    double av_time = total_gap_time/(Dot_Num-1);
-    double freq = (Dot_Num-1)/total_gap_time;
-    double throughput = ((Dot_Num-1)*LBA_NUM/2)/total_gap_time;
+    FILE *file = fopen(LOG_FILE, "wb");
+    fwrite(timestamps, sizeof(uint64_t), NUM_READ_OPS, file);
+    fclose(file);
 
-    printf("Operation Average time: %.3les\n" , av_time);
-    printf("IOPS: %15lf\n", freq);
-    printf("Throughput(GB/s): %lfM, %lfG\n", throughput/1024, throughput/1024/1024);
-	return;
+    uint64_t total_time = timestamps[NUM_READ_OPS - 1] - timestamps[0];
+    double total_seconds = total_time / CPU_HZ;
+    double avg_time_per_op = total_seconds / (NUM_READ_OPS - 1);
+    double iops = (NUM_READ_OPS - 1) / total_seconds;
+    double throughput_gb = ((NUM_READ_OPS - 1) * LBA_BLOCKS / 2) / total_seconds / 1024 / 1024;
+
+    printf("Average operation time: %.3e seconds\n", avg_time_per_op);
+    printf("IOPS: %.3f\n", iops);
+    printf("Throughput: %.3f GB/s\n", throughput_gb);
 }
 
-int main(int argc, char **argv)
-{
-	int rc;
-	struct spdk_env_opts opts;
-	/*
-	 * SPDK relies on an abstraction around the local environment
-	 * named env that handles memory allocation and PCI device operations.
-	 * This library must be initialized first.
-	 *
-	 */
-	spdk_env_opts_init(&opts);
-	opts.name = "hello_world";
-	opts.shm_id = 0;
-	if (spdk_env_init(&opts) < 0) {
-		fprintf(stderr, "Unable to initialize SPDK env\n");
-		return 1;
-	}
-	printf("Initializing NVMe Controllers\n");
+int main(int argc, char **argv) {
+    struct spdk_env_opts opts;
+    spdk_env_opts_init(&opts);
+    opts.name = "hello_world";
+    opts.shm_id = 0;
 
-	/*
-	 * Start the SPDK NVMe enumeration process.  probe_cb will be called
-	 *  for each NVMe controller found, giving our application a choice on
-	 *  whether to attach to each controller.  attach_cb will then be
-	 *  called for each controller after the SPDK NVMe driver has completed
-	 *  initializing the controller we chose to attach.
-	 */
-	rc = spdk_nvme_probe(NULL, NULL, probe_cb, attach_cb, NULL);
-	if (rc != 0) {
-		fprintf(stderr, "spdk_nvme_probe() failed\n");
-		cleanup();
-		return 1;
-	}
+    if (spdk_env_init(&opts) < 0) {
+        fprintf(stderr, "Error: Failed to initialize SPDK environment.\n");
+        return 1;
+    }
 
-	if (g_controllers == NULL) {
-		fprintf(stderr, "no NVMe controllers found\n");
-		cleanup();
-		return 1;
-	}
+    printf("Initializing NVMe controllers...\n");
+    if (spdk_nvme_probe(NULL, NULL, probe_cb, attach_cb, NULL) != 0) {
+        fprintf(stderr, "Error: Failed to probe NVMe controllers.\n");
+        cleanup();
+        return 1;
+    }
 
-	printf("Initialization complete.\n");
-    Probe();
-	cleanup();
-	return 0;
+    if (!g_controllers) {
+        fprintf(stderr, "Error: No NVMe controllers found.\n");
+        cleanup();
+        return 1;
+    }
+
+    printf("Initialization complete.\n");
+    run_probe();
+    cleanup();
+    return 0;
 }
--- show_the_logs/show_line_nvme.py
+++ show_the_logs/show_line_nvme.py
@@ -1,18 +1,31 @@
 #!/usr/bin/python3
 
 from utils import *
+import sys
+import matplotlib.pyplot as plt
 
-filename = "records.bin"
+if not (len(sys.argv) > 1):
+    print("argument not provided")
+    sys.exit(1)
+
+filename = "../../nvme_probe/records.bin"
 
 data = read_records_nvme(filename)
+# print(len(data))
+x1, y1 = [], []
 
-x1, y1 =[], []
-
-for i in range(0, len(data)-1):
+for i in range(0, len(data) - 1):
+    m = data[i + 1] - data[i]
     x1.append(i)
-    y1.append(data[i+1] - data[i])
-
+    y1.append(m)
+print(len(x1))
 fig = plt.figure(1)
 ax = fig.add_subplot(111)
-ax.plot(x1,y1, linewidth=0.25)
-plt.show()
+ax.plot(x1, y1, linewidth=0.25)
+
+ax.set_xlabel("Time")
+ax.set_ylabel("Number of cycles")
+ax.set_ylim(0, 350000)
+
+filename = "nvme" + str(sys.argv[1]) + ".png"
+# plt.savefig(filename, dpi=300)
